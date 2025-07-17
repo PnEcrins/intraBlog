6983a841f40fdc9cf1ec9619e33d2d6306f5ad0b
TheoLechemia <lechemia.theo@gmail.com>
2025-07-17 14:07:38 +0200

    set admin to root

--- intraBlog/urls.py
+++ intraBlog/urls.py
@@ -6,7 +6,6 @@ from blog.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 from rest_framework.routers import DefaultRouter
 
 
@@ -16,12 +15,10 @@ router.register("categories", CategoryViewSet, "category")
 
 urlpatterns = (
     [
+        path("", admin.site.urls),
         path("i18n/", include("django.conf.urls.i18n")),
         path("api/", include(router.urls)),
         path("tinymce/", include("tinymce.urls")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + i18n_patterns(
-        path("admin/", admin.site.urls),
-    )
 )
