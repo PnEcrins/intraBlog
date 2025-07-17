8775e85a3c1db0026294a5a9027b3b1247346bde
TheoLechemia <lechemia.theo@gmail.com>
2025-07-17 12:51:26 +0200

    remove translation

--- blog/models.py
+++ blog/models.py
@@ -15,28 +15,28 @@ class PostManager(models.Manager):
 
 
 class Category(models.Model):
-    name = models.CharField(verbose_name=_("Category"), max_length=100, unique=True)
+    name = models.CharField(verbose_name="Categorie", max_length=100, unique=True)
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name_plural = "Catégories"
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(verbose_name=_("Title"), max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Author"))
-    content = tiny_mce.HTMLField(verbose_name=_("Content"), blank=True)
-    created_at = models.DateTimeField(verbose_name=_("created_at"), auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name=_("updated_at"), auto_now=True)
-    categories = models.ManyToManyField(Category, verbose_name=_("Category"))
-    posted = models.BooleanField(default=False, verbose_name=_("Posted"))
+    title = models.CharField(verbose_name="Titre", max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Auteur")
+    content = tiny_mce.HTMLField(verbose_name="Contenu", blank=True)
+    created_at = models.DateTimeField(verbose_name="Créé le", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Éditer le", auto_now=True)
+    categories = models.ManyToManyField(Category, verbose_name="Catégorie")
+    posted = models.BooleanField(default=False, verbose_name="Publié")
     image = models.ImageField(
-        upload_to="post_images/", null=True, blank=True, verbose_name=_("Image")
+        upload_to="post_images/", null=True, blank=True, verbose_name="Image"
     )
     file = models.FileField(
-        upload_to="post_files/", null=True, blank=True, verbose_name=_("File")
+        upload_to="post_files/", null=True, blank=True, verbose_name="Fichier"
     )
 
     objects = PostManager()
